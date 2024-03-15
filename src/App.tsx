a122e890d708d22e74a207c24480222b3a98c30b
Paul Gasbarra <paulgasbarra@gmail.com>
2024-03-14 22:18:07 -0400

    added a component for projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import projects from "./data/projects.json";
+import Project from "./component/project";
 
 function App() {
   return (
@@ -19,67 +21,22 @@ function App() {
         </div>
       </div>
 
-      <section id="work">
-        <div className="work-experience">
-          <div className="work-desc">
-            <h2>Stacks & Joules</h2>
-            <p>Static site for non-profit education.</p>
-            <a href="https://github.com/repo-link">GitHub Repo</a>
-          </div>
+      <div className="divider">Projects</div>
+
+      {projects &&
+        projects.projects.map((project) => (
+          <Project key={project.id} project={project} />
+        ))}
+
+      <div className="work-experience">
+        <div className="work-desc">
+          <h2>The Muse</h2>
+          <p>Interactive job hunt advice site.</p>
+        </div>
+        <a href="https://www.themuse.com">
           <img src="path-to-work-image1.jpg" alt="Work Description" />
-        </div>
-
-        <div className="work-experience">
-          <div className="work-desc">
-            <h2>The Muse</h2>
-            <p>Interactive job hunt advice site.</p>
-          </div>
-          <a href="https://www.themuse.com">
-            <img src="path-to-work-image1.jpg" alt="Work Description" />
-          </a>
-        </div>
-
-        <div className="work-experience">
-          <div className="work-desc">
-            <h2>Intersection</h2>
-            <p>
-              Building out digital infrastructure for Transit Organizations
-              across the country
-            </p>
-            <a href="https://github.com/repo-link">GitHub Repo</a>
-          </div>
-          <img src="path-to-work-image1.jpg" alt="Work Description" />
-        </div>
-      </section>
-
-      <div className="divider">Side Projects</div>
-
-      <section id="side-projects">
-        <div className="work-experience">
-          <div className="work-desc">
-            <h2>Insta Haiku</h2>
-            <p>A robot that writes haiku daily</p>
-            <a href="https://github.com/repo-link">GitHub Repo</a>
-          </div>
-          <img src="images/haiku.png" alt="Work Description" />
-        </div>
-        <div className="work-experience">
-          <div className="work-desc">
-            <h2>Dragons Before Dinner</h2>
-            <p>Adventure Card Game</p>
-            <a href="https://github.com/repo-link">GitHub Repo</a>
-          </div>
-          <img src="images/wizard.png" alt="Work Description" />
-        </div>
-        <div className="work-experience">
-          <div className="work-desc">
-            <h2>Portfolio Info Graphic Generator</h2>
-            <p>How many things have you thunged</p>
-            <a href="https://github.com/repo-link">GitHub Repo</a>
-          </div>
-          <img src="images/linkedin_profile.png" alt="Work Description" />
-        </div>
-      </section>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/component/project.tsx
@@ -0,0 +1,25 @@
+interface ProjectProps {
+  project: {
+    id: number;
+    name: string;
+    image: string;
+    imageAlt: string;
+    description: string;
+    githubLink: string;
+    websiteLink: string;
+  };
+}
+
+const Project: React.FC<ProjectProps> = ({ project }) => {
+  return (
+    <div>
+      <h1>{project.name}</h1>
+      <img src={project.image} alt={project.imageAlt} />
+      <p>{project.description}</p>
+      <a href={project.githubLink}>GitHub Repo</a>
+      {project.websiteLink && <a href={project.websiteLink}>Live Site</a>}
+    </div>
+  );
+};
+
+export default Project;
